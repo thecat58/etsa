c8009a10aecee469209c8a3d58ffa781c30b7d7c
thecat58 <mkike518@gmail.com>
2023-05-12 19:17:42 -0500

    se actualizo admin.py en la app principal

--- principal/admin.py
+++ principal/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 from principal.models import *
 # Register your models here.
 admin.site.register(Tdoc)
+admin.site.register(Cateserv)
+admin.site.register(Ctaller)
+admin.site.register(Municipio)
+admin.site.register(Scripcion)
+admin.site.register(Tipersona)
+
+
